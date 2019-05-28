9d750594623c343c8e43dd011e4204de34cd980b
Cesar Mejia <crsmejia93@gmail.com>
2019-05-28 11:06:18 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_401/Springboot401Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_401;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot401Application.class, args);
+	}
+
+}
