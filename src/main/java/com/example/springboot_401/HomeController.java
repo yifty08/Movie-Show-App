2c57ed671bd79a3c3318230eebe67e756b3aa288
Cesar Mejia <crsmejia93@gmail.com>
2019-05-29 14:41:22 -0400

    pushing all changes

--- src/main/java/com/example/springboot_401/HomeController.java
+++ src/main/java/com/example/springboot_401/HomeController.java
@@ -1,7 +1,6 @@
 package com.example.springboot_401;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
